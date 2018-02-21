31b4fac54d28eb1ebe8d923170db085463ccfac7
corentin.gautier <corentin.gautier@localhost.localdomain>
2018-02-21 15:48:27 +0100

    printf for %x

--- corewar/src/cor_to_tab.c
+++ corewar/src/cor_to_tab.c
@@ -5,6 +5,7 @@
 ** cor_to_tab
 */
 
+#include <stdio.h> /* tmp */
 #include <unistd.h>
 #include <fcntl.h>
 #include "prototype.h"
@@ -28,9 +29,9 @@ bool cor_to_tab(char *tab, char *file_name, int address)
 	}
 	while (n == sizeof(char));
 	close(fd);
-	for (i = 0; i < MEM_SIZE; i++) {         /* tmp */
-		my_printf("%x ", tab[i]);       /* tmp */
-	}                                      /* tmp */
-	my_printf("\n");                      /* tmp */
+	for (i = 0; i < MEM_SIZE; i++) {      /* tmp */
+		printf("%x ", tab[i]);       /* tmp */
+	}                                   /* tmp */
+	printf("\n");                      /* tmp */
 	return (true);
 }
