52ec2c213ff7ba85d77641bd4a641ec501f89d5c
corentin.gautier <corentin.gautier@localhost.localdomain>
2018-02-22 13:05:53 +0100

    modif corewar.c

--- corewar/src/corewar.c
+++ corewar/src/corewar.c
@@ -26,7 +26,7 @@ int corewar(char *tab, size_t nb_player, player_t *player)
 	while (1) {
 		for (size_t i = 0; i < nb_player; i++)
 			player_play(tab, player + i);
-		for (size_t i = 0; i < nb_player; i++)
+//		for (size_t i = 0; i < nb_player; i++)
 	}
 	return (0);
 }
