d40fbb64400c8c7f9557e4305f20e9f52e65119a
Pacôme Françon <pacome.francon@epitech.eu>
2018-02-23 15:46:35 +0100

    check_nbr_arg.c ok

---
+++ asm/src/parser/check_nbr_arg.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2018
+** check_nbr_arg
+** File description:
+** check_nbr_arg
+*/
+
+#include <stdbool.h>
+#include "op.h"
+
+bool check_nbr_arg(char id, char *line)
+{
+	int i = 0;
+	int nb_arg = 0;
+
+	if (id < 0 || line == NULL)
+		return (false);
+	while (line[i] != '\0') {
+		if (line[i] == SEPARATOR_CHAR)
+			nb_arg++;
+		i++;
+	}
+	if (op_tab[id].nbr_args != nb_arg + 1)
+		return (false);
+	return (true);
+}
