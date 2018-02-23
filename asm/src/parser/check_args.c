56aea57c05bdbcb2f04974406181a12c657a2862
Pacôme Françon <pacome.francon@epitech.eu>
2018-02-23 16:24:37 +0100

    fichier check_argc.c

---
+++ asm/src/parser/check_args.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2018
+** check_args
+** File description:
+** check_args
+*/
+
+#include "op.h"
+
+/*
+** Vérifie si les arguments passés sont bons en fonction du premier caractère
+** après la virgule
+** Une fois que les arguments sont correctes, on calcule le coding byte et hop
+*/
+
+char check_args(char **line)
+{
+	char coding_byte = 0;
+
+	return (true);
+}
