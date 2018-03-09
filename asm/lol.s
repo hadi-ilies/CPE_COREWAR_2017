b535e33b22287e63b1cd9784024261935b69e473
Pacôme Françon <pacome.francon@epitech.eu>
2018-03-09 19:45:09 +0100

    suppression fichiers inutiles

--- asm/lol.s
+++
@@ -1,8 +0,0 @@
-.name "zork"
-.comment "just a basic living prog"
-
-mdr:	ld 10, r1
-
-lol:	zjmp %0
-
-mdr:	ld 10 r1
--- asm/zork.s
+++
@@ -1,8 +0,0 @@
-.name "zork"
-.comment "Un champion avec un commentaire, c'est un bon champion."
-
-12:     sti r1, %:live, %1
-	and r1 ,%0, r1
-
-live:   live %1
-	zjmp %:live
